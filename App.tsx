d80f1945929b0423c8c615890d90eb8499287c94
김소연 <gimsoyeon@gimsoyeons-MacBook-Pro.local>
2022-09-30 10:49:34 +0900

    :tada: Init project

---
+++ App.tsx
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React, {type PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  Text,
+  View,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text> Init </Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
